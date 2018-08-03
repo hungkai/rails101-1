22063e150af6892b11df5c4cc9e36a826492f753
hungkai <hungkai.yang@gmail.com>
2018-08-03 09:54:44 +0800

    find_post & member_require to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_post, only: [:edit, :update, :destroy]
+  before_action :member_required, only: [:new, :create]
 
   def new
     @group = Group.find(params[:group_id])
@@ -19,12 +21,9 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:group_id])
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to account_posts_path, notice: "Update Success"
     else
@@ -33,12 +32,24 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to account_posts_path, alert: "Post deleted"
   end
 
   private
+
+  def member_required
+    @group = Group.find(params[:group_id])
+    if !current_user.is_member_of?(@group)
+      redirect_to group_path(@group)
+      flash[:warning] = "你不是這個討論版的成員，不能發文！"
+    end
+  end
+
+  def find_post
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:content)
